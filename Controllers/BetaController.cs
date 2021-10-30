983c173af02e8a114d11ecfdaa4598e19e1dde00
nf-tw <63173805+nf-tw@users.noreply.github.com>
2021-10-30 16:39:53 -0700

    Added Beta controller, view and layout

---
+++ Controllers/BetaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tellyt.Controllers
+{
+    public class BetaController : Controller
+    {
+        // GET: Beta
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
