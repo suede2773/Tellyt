f415be1bdefb4f1425fc1fc4f6f9c1c5ccbbd3b5
nf-tw <63173805+nf-tw@users.noreply.github.com>
2022-12-31 15:35:52 -0800

    v2

---
+++ Controllers/v2/v2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tellyt.Controllers._2
+{
+    public class v2Controller : Controller
+    {
+        // GET: v2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Scripts/v2/kendo_init.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    onHandleKendoInit();
+    setTimeout(onHandleKendoInit, 500);
+});
+
+function onHandleKendoInit() {
+    handleKendoButtons();
+}
+
+function handleKendoButtons() {
+    $(".tlyt_kendo_button").each(function (index) {
+        if (!$(this).hasClass("tlyt_kendo_initialized")) {
+            $(this).kendoButton();
+            $(this).addClass("tlyt_kendo_initialized");
+        }
+    });
+}
+
+function handleDropdownListInputs() {
+    $(".tlyt_dropdownlist").each(function (index) {
+        if (!$(this).hasClass("tlyt_kendo_initialized")) {
+            //$(this).kendoDropDownList();
+            $(this).addClass("tlyt_kendo_initialized");
+        }
+    });
+}
